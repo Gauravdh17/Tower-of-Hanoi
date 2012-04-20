b8dbf05585d640488829b91b61ceabac53aa9d95
Gaurav D H <hunuswadkar.gaurav@gmail.com>
2012-04-20 17:07:27 +0530

    second commit

---
+++ Stack.rb
@@ -0,0 +1,47 @@
+class Stack
+  
+  
+  def initialize()
+    @stk = Array.new
+	@count = -1
+	@lens = 5
+  end
+  
+  # Method to add an element in the stack
+  def push(ele)
+  
+    if(@stk.size == nil)
+	  p "Stack full"
+	else
+      @count = @count + 1
+	  @stk[@count] = ele	  
+	end
+  end
+  
+  #method to delete an element in the stack
+  def pop
+   if(@stk.empty?)
+	  p "Stack empty"
+	else
+	  @stk[@count] = nil
+	  @count = @count - 1
+	end
+  end
+  
+  #display all the elements in the stack
+  def display
+    p @stk
+  end
+  
+  #get the element at the top of stack
+  def lastElement
+    return @stk[@count]
+  end
+  
+  #Method to add the elements at the start just to initialize the stack
+  def init(n)
+    for s in 1..n do
+	  @stk.push(s)
+	end
+  end
+end
---
+++ toh2.rb
@@ -0,0 +1,69 @@
+require "Stack.rb"
+
+def tower( n, frompeg, topeg, auxpeg )
+  if n == 1
+  
+    #If only one element, move the element and return
+    temp = frompeg.lastElement
+    topeg.push(temp)
+	frompeg.pop
+	
+	print " from "
+	frompeg.display
+	print " to "
+	topeg.display
+	print " aux "
+	auxpeg.display
+	p "------------------------"
+	
+  else
+    
+    #Move top (n-1) elements from A to B using C as auxilary
+    tower( n-1, frompeg, auxpeg, topeg )
+	temp2 = frompeg.lastElement
+	topeg.push(temp2)
+	frompeg.pop
+	
+	print " from "
+	frompeg.display
+	print " to "
+	topeg.display
+	print " aux "
+	auxpeg.display
+	p "------------------------"
+	
+	#Move top (n-1) elements from B to C using A as auxilary
+	tower( n-1,  auxpeg, topeg, frompeg )	
+  end  
+end
+
+
+A = Stack.new
+B = Stack.new
+C = Stack.new
+
+print("Enter the number of elements in the stack ")
+n = gets().to_i
+A.init(n)
+
+p "***************************"
+p "Initial values"
+print "\nA = "
+A.display
+print "\nB = "
+B.display
+print "\nC = "
+C.display
+p "***************************"
+
+tower( n, A, C , B )
+
+p "***************************"
+p "Values after the final move"
+print "\nA = "
+A.display
+print "\nB = "
+B.display
+print "\nC = "
+C.display
+p "***************************"
