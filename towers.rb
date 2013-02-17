6abb26f38d19c866338aaf55bef34eeb7a0db96f
Douglas G. Allen <kb9agt@gmail.com>
2013-02-17 01:56:39 -0600

    got towers.rb working

--- towers.rb
+++ towers.rb
@@ -1,16 +1,16 @@
 
 def printMove(fr, to)
-  "move disk at peg #{fr} to peg #{to}."
+  puts "move disk at peg #{fr} to peg #{to}."
 end
 
 def towers(n, fr, to, spare)  
   if n == 1
     printMove(fr, to)
   else
-    towers(n-1, fr, spare, to)          
-    towers(1, fr, to, spare)        
-    towers(n-1, spare, to, fr)
+    towers(n-1, fr, spare, to)       
+    towers(1, fr, to, spare)    
+    towers(n-1, spare, to, fr)    
   end
 end
 
-puts towers(2, "A", "B", "C")
+towers(2, "A", "B", "C")
